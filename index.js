813ebdabd8869b74bab6c7485fae8ec1f5681572
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2018-11-08 04:49:35 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// JavaScript File
+"use strict";
+
+window.onload = function(){
+            let button = document.querySelector("button");
+            button.addEventListener("click", function(){
+                alert("Working");
+            });
+        }
