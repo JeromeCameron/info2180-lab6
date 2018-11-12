7866491ac93961d6ef4273ec6c1fed05db6162c5
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2018-11-12 00:05:14 +0000

    exercise 4 complete

--- index.js
+++ index.js
@@ -2,8 +2,60 @@
 "use strict";
 
 window.onload = function(){
-            let button = document.querySelector("button");
-            button.addEventListener("click", function(){
-                alert("Working");
-            });
-        }
+            
+            let searchOne = document.getElementById("searchOne");
+            let showAll = document.getElementById("showAll");
+            let httpRequest;
+        
+            searchOne.addEventListener("click", makeRequest);
+            showAll.addEventListener("click", makeRequestAll);
+                
+            function makeRequest(){
+                let searchWord = document.querySelector("input").value;
+                httpRequest = new XMLHttpRequest();
+                httpRequest.onreadystatechange = request;
+                httpRequest.open('GET', "/request.php?q=" + searchWord + "&all = false", true);
+                httpRequest.send();
+            }
+            
+            function request(){
+                if (httpRequest.readyState === XMLHttpRequest.DONE){
+                    if (httpRequest.status === 200){
+                        document.getElementById('result').innerHTML = httpRequest.responseText;
+                    } else {
+                        alert('There was a problem with the request.');
+                    }
+                }
+            }
+            
+            function makeRequestAll(){
+                httpRequest = new XMLHttpRequest();
+                httpRequest.onreadystatechange = requestAll;
+                httpRequest.open('GET', "/request.php?q=&all=true", true);
+                httpRequest.send();
+            }
+            
+             function requestAll(){
+                if (httpRequest.readyState === XMLHttpRequest.DONE){
+                    if (httpRequest.status === 200){
+                        let xmlData = httpRequest.responseXML;
+                        let list = document.createElement("ol");
+                        let parent = document.getElementById("result");
+                        document.getElementById('result').innerHTML = "";
+                        parent.appendChild(list);
+                        let xmlNodes = xmlData.getElementsByTagName("definition");
+        
+                        for(let i = 0; i < xmlNodes.length; i++){
+                            let listItem = document.createElement("li");
+                            let txt = "<h3>" + xmlNodes[i].getAttribute("name") + "</h3>" + 
+                            "<br>" + "<p>" + xmlNodes[i].childNodes[0].nodeValue + "</p>" +
+                            "<br>" + "<p>" + "-" + xmlNodes[i].getAttribute("author") + "</p>";
+                            listItem.innerHTML = txt;
+                            list.appendChild(listItem);
+                        }
+                    }else {
+                        alert('There was a problem with the request.');
+                    }
+                }
+            }
+};
--- request.php
+++ request.php
@@ -1,15 +1,53 @@
 <?php
 // accept a term (keyword)
 // respond with a value
+
 $query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$query_2 = $_GET['all'];
+
+
+ $definition = [
+     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
+     "bar" => "A place that sells alcholic beverages",
+     "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
+     "html" => "The standard markup language for creating web pages and web applications.",
+     "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
+     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
+     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+ ];
+
+ if ($query_2 == "true"){
+    
+     $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+    <entries>
+        <definition name ="definition" author="John">
+             "A statement of the exact meaning of a word, especially in a dictionary."
+        </definition>
+        <definition name ="bar" author="mary">
+             "A place that sells alcholic beverages"
+        </definition>
+        <definition name ="ajax" author="kimberly">
+             "Technique which involves the use of javascript and xml (or JSON)"
+        </definition>
+        <definition name ="html" author="mark">
+             "The standard markup language for creating web pages and web applications."
+        </definition>
+        <definition name ="css" author="Jude">
+             "A style sheet language used for describing the presentation of a document written in a markup language."
+        </definition>
+        <definition name ="javascript" author="Sasha">
+             "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website."
+        </definition>
+        <definition name ="php" author="Amy">
+             "A server-side scripting language, and a powerful tool for making dynamic and interactive websites"
+        </definition>
+    </entries>';
+
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+   
+}else{
+     print("<h3>" . strtoupper($query) . "</h3>");
+     print("<p>" . $definition[$query] . "</p>");
+ }
